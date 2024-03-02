6db8305ce6a22db257ecb7bbb331b08601e77f17
Harshit Raj <iamrajharshit@gmail.com>
2024-03-03 00:11:43 +0530

    move zeros

---
+++ leet/02_Move Zeros.py
@@ -0,0 +1,23 @@
+#Given an array nums, write a function to move all 0's 
+# to the end of it while maintaining the relative order of the 
+#non-zero elements.
+
+#in-place
+#sol in O(n)
+
+#if zero is countered hold it and swap with a number
+# pointer1 for loop i=0 to end
+# pointer2 prev=0
+a=[1,0,3,0,4,0,5]
+def sol(a):
+    pre_indx=0 #p2 and i=p1
+    for i in range(0,len(a)):
+        if a[i]!=0:
+            hold=a[pre_indx]
+            a[pre_indx]=a[i]
+            a[i]=hold
+            pre_indx=pre_indx+1
+
+print(a)
+x=sol(a)
+print(a)            
