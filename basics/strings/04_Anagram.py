3678d9f074c77800ac13e933445bbdd8665356de
Harshit Raj <iamrajharshit@gmail.com>
2024-03-27 22:34:31 +0530

    list

--- basics/strings/04_Anagram.py
+++ basics/strings/04_Anagram.py
@@ -1,3 +1,32 @@
+'''Anagram
+What is an Anagrams?
+-Both strings should be of equal length.
+-Both strings should have same chars in same order or diff order.
+
+ex: ABC BAC both are anagram! as length are same and have same set of chars.
+
+
+'''
+#####################################################
+#Simple code
+# str1=input("Enter string 1")
+# str2=input("Enter string 2")
+
+# if len(str1)!=len(str2):
+#     print("not")
+
+# else:
+#     if sorted(str1) == sorted(str2):
+#         print("s")
+#     else:
+#         print("not")
+
+
+
+
+
+#list
+####################################################
 from collections import defaultdict
 
 class Solutions:
@@ -16,7 +45,7 @@ class Solutions:
 
 strs=['ert','tre','fgh','ggf','yui','iuy']          
 s=Solutions()
-print(s.grAnagram(strs))
+#print(s.grAnagram(strs))
 
 
 
