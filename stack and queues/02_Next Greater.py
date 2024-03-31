933010f18987a41a36be2ac7b2f90e2a395969e6
Harshit Raj <iamrajharshit@gmail.com>
2024-03-31 21:45:44 +0530

    nextGen

---
+++ stack and queues/02_Next Greater.py
@@ -0,0 +1,17 @@
+'''Givrn array, find the next greater element G[i] for every element A[i]
+in array. The Next greater Element for an element A[i] is the greater element on 
+the right side of A[i] in array.
+
+Input: [4,5,2,10]
+Output:[5,10,10,-1]
+
+Input:[3,2,1]
+Output:[-1,-1,-1]
+'''
+def nextGen(A):
+    pass
+        
+
+A=[4,5,2,10]    
+x=nextGen(A)
+print(x)
