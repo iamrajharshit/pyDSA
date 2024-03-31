de778db1b8274455e17b7412688ed1c703fcdaee
Harshit Raj <iamrajharshit@gmail.com>
2024-03-31 23:30:48 +0530

    Next greatest number

--- stack and queues/02_Next Greater.py
+++ stack and queues/02_Next Greater.py
@@ -9,8 +9,24 @@ Input:[3,2,1]
 Output:[-1,-1,-1]
 '''
 def nextGen(A):
-    pass
-        
+    n = len(A)  # Length of the input array
+    stack = []  # Initialize an empty stack to hold elements and their indices
+    nge = [-1] * len(A)  # Initialize an array to store the next greater elements, initialized with -1
+
+    # Iterate over the elements of the input array along with their indices
+    for i, A in enumerate(A):
+        # While the stack is not empty and the top element of the stack is less than the current element
+        while stack and stack[-1][0] < A:
+            # Pop the top element from the stack and update its next greater element to the current element
+            nge[stack.pop()[1]] = A
+
+        # Push the current element and its index onto the stack
+        stack.append((A, i))
+
+    # Return the array containing the next greater elements
+    return nge
+
+
 
 A=[4,5,2,10]    
 x=nextGen(A)
