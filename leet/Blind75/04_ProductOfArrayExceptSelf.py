08c30bbdd55c983293d82f004b3d8ad54753f7db
iamrajharshit <iamrajharshit@gmail.com>
2024-10-02 01:23:00 +0530

    Blind75 q4

--- leet/Blind75/04_ProductOfArrayExceptSelf.py
+++ leet/Blind75/04_ProductOfArrayExceptSelf.py
@@ -1,4 +1,25 @@
 '''
 Product of array expect self.
 
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+Each product is guaranteed to fit in a 32-bit integer.
+
 '''
+
+arr=[1,2,3,4]
+# output= [24,12,8,6]
+
+res=[1]*len(arr)
+
+pre =1
+for i in range(len(arr)):
+    res[i]=pre
+    pre*=arr[i]
+
+post=1
+for i in range(len(arr)-1,-1,-1):
+    res[i]*=post
+    post*=arr[i]
+
+print(res)
