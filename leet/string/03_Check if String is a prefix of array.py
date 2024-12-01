1c3116d8110c41d4dabb09f80a2ba0081641f55b
iamrajharshit <iamrajharshit@gmail.com>
2024-12-02 00:09:04 +0530

    easy string ques

---
+++ leet/string/03_Check if String is a prefix of array.py
@@ -0,0 +1,20 @@
+
+'''
+Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+Return true if s is a prefix string of words, or false otherwise.
+
+'''
+
+
+
+def isPrefixString(self, s: str, words: List[str]) -> bool:
+    ans=""
+    for i in range(len(words)):
+        ans+=words[i]
+        if ans == s:
+            return True
+    return False
+    
